9ef38b7ae525e3903fd2253f4f9c97d1241f760b
Chris <Chris.m.simon1@gmail.com>
2026-05-10 20:57:37 -0400

    added in-memory database for basic functionality

--- src/main/java/com/csimon_dev/health_dashboard/HealthDashboardApplication.java
+++ src/main/java/com/csimon_dev/health_dashboard/HealthDashboardApplication.java
@@ -3,11 +3,8 @@ package com.csimon_dev.health_dashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Just to cheese making it run properly on my first start
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
-// Remove the (exclude...) everything within the brackets later
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+// Currently points to our in-memory(h2) database
+@SpringBootApplication
 public class HealthDashboardApplication {
 
 	public static void main(String[] args) {
